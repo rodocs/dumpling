74283c2bc49130c8d8ad59f7118609fd6f822441
Lucien Greathouse <me@lpghatguy.com>
2018-10-11 00:01:26 -0700

    Fix camelCase deprecation heuristic to be more accurate

--- src/heuristics.rs
+++ src/heuristics.rs
@@ -16,18 +16,31 @@ fn make_first_letter_uppercase(input: &str) -> String {
 /// Members with camelCase names are probably deprecated.
 pub fn camelcase_members_probably_deprecated(dump: &mut Dump) {
     for class in dump.classes.iter_mut() {
-        for member in class.members.iter_mut() {
+        let mut fixups: Vec<(usize, String)> = Vec::new();
+
+        for (index, member) in class.members.iter().enumerate() {
             let first_char = member.get_name().chars().nth(0).unwrap();
 
             if first_char.is_lowercase() {
-                let description = {
-                    let name = member.get_name();
-                    let fixed_name = make_first_letter_uppercase(name);
+                let fixed_name = make_first_letter_uppercase(member.get_name());
 
-                    format!("`{}` is camelCase and is probably deprecated. Consider using `{}` instead if it exists.", name, fixed_name)
-                };
-                member.set_description(description);
+                // We should make sure a PascalCase version exists!
+                let has_pascal_version = class.members
+                    .iter()
+                    .position(|member| member.get_name() == fixed_name)
+                    .is_some();
+
+                if has_pascal_version {
+                    fixups.push((index, fixed_name));
+                }
             }
+        }
+
+        for (index, fixed_name) in &fixups {
+            let member = &mut class.members[*index];
+
+            let description = format!("`{}` is deprecated. Use `{}` instead.", member.get_name(), fixed_name);
+            member.set_description(description);
         }
     }
 }
